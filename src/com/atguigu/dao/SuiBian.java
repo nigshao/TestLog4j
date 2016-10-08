4394563b3dd72f6983a3d3ce4bbac7387221efe3
nigshao <1515467166@qq.com>
2016-10-08 21:48:16 +0800

    Update SuiBian.java

--- src/com/atguigu/dao/SuiBian.java
+++ src/com/atguigu/dao/SuiBian.java
@@ -1,8 +1,10 @@
 package com.atguigu.dao;
 
 public class SuiBian {
-   Sysotem.out.println("1111");
-   Sysotem.out.println("2222");
-   Sysotem.out.println("3333");
-   Sysotem.out.println("4444");
+   public void test1(){
+      Sysotem.out.println("1111");
+      Sysotem.out.println("2222");
+      Sysotem.out.println("3333");
+      Sysotem.out.println("4444");
+      }
 }

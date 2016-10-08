a71902fcbbeb56fb7c162601bab05e2d13185651
ningshao <1515467166@qq.com>
2016-10-08 21:42:44 +0800

    zzz

--- src/com/atguigu/dao/SuiBian.java
+++ src/com/atguigu/dao/SuiBian.java
@@ -1,5 +1,12 @@
 package com.atguigu.dao;
 
 public class SuiBian {
+	public static void main(String[] args) {
+		System.out.println("aaaaa");
+		System.out.println("bbbbb");
+		System.out.println("ccccc");
+		System.out.println("ddddd");
+	}
+	
 
 }

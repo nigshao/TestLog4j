068843a3ecad2f00c12492b055cb087d1f1f8952
nigshao <1515467166@qq.com>
2016-10-08 21:46:02 +0800

    Update SuiBian.java
    github上改变的

--- src/com/atguigu/dao/SuiBian.java
+++ src/com/atguigu/dao/SuiBian.java
@@ -1,5 +1,8 @@
 package com.atguigu.dao;
 
 public class SuiBian {
-
+   Sysotem.out.println("1111");
+   Sysotem.out.println("2222");
+   Sysotem.out.println("3333");
+   Sysotem.out.println("4444");
 }

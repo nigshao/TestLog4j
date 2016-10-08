b6c25c5d975df621d791fd9a47ad1231e98bc0f8
nigshao <1515467166@qq.com>
2016-10-08 22:07:30 +0800

    Update SuiBian.java

--- src/com/atguigu/dao/SuiBian.java
+++ src/com/atguigu/dao/SuiBian.java
@@ -13,6 +13,6 @@ public class SuiBian {
 	   System.out.println("1111");
 	   System.out.println("2222");
 	   System.out.println("3333");
-	   System.out.println("4444");
+	   System.out.println("444444444");
       }
 }
